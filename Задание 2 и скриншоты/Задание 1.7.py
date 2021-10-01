c152dce09698527e6ccd4703b4c496daaead0942
vaburavlev <90718147+vaburavlev@users.noreply.github.com>
2021-10-01 17:44:25 +0300

    Исправлено

--- Задание 2 и скриншоты/Задание 1.7.py
+++ Задание 2 и скриншоты/Задание 1.7.py
@@ -1,10 +1,6 @@
 # --coding: utf -8 --
-year = int(input('Введите год'))
-if (year % 4 == 0):
-	print("Высокосный")
-elif (year%100 != 0):
-	print("Высокосный")
-elif (year%400 == 0): 
+year = int(input('Ведите первый год'))
+if (year % 4 == 0 and year%100 != 0) or (year%400 == 0):
     print("Высокосный")
 else:
     print("Обычный")
