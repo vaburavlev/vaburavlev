30846fbd8b6648096cd2ea53d686ebddcf8a2186
vaburavlev <90718147+vaburavlev@users.noreply.github.com>
2021-10-01 18:19:11 +0300

    Исправлено

--- Задание 2 и скриншоты/Задание 1.5.py
+++ Задание 2 и скриншоты/Задание 1.5.py
@@ -1,10 +1,9 @@
 # --coding: utf -8 --
-a = int(input('Введите первое число'))
-b = int(input('Введите второе число'))
-c = int(input('Введите третье число'))
-if b >= a <= c:
-    print(a)
-elif a >= b <= c:
-    print(b)
-else:
-    print(c)
+def w():
+    print("Введите входные данные (3 числа)")
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    print("Ответ:")
+    return min(x,y,z)
+print(w())
