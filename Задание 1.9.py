2316a25a2f97c415dfbc8a1e4931dfa1f1e97d92
vaburavlev <90718147+vaburavlev@users.noreply.github.com>
2021-10-01 18:48:48 +0300

    Delete Задание 1.9.py

--- Задание 1.9.py
+++
@@ -1,8 +0,0 @@
-# --coding: utf -8 --
-n = int(input('Ведите первое число'))
-m = int(input('Ведите второе число'))
-k = int(input('Ведите третье число'))
-if k < n * m and ((k % n == 0) or (k % m == 0)):
-    print('ДА')
-else:
-    print('НЕТ')
