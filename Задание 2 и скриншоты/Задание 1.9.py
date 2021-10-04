2f577f89a6e74ec25facdb2f0aae5c1cb333d352
vaburavlev <90718147+vaburavlev@users.noreply.github.com>
2021-10-04 15:19:06 +0300

    Исправленно

--- Задание 2 и скриншоты/Задание 1.9.py
+++ Задание 2 и скриншоты/Задание 1.9.py
@@ -1,8 +1,11 @@
-# --coding: utf -8 --
-n = int(input('Ведите первое число'))
-m = int(input('Ведите второе число'))
-k = int(input('Ведите третье число'))
-if k < n * m and ((k % n == 0) or (k % m == 0)):
-    print('ДА')
-else:
-    print('НЕТ')
+# -*- coding: utf-8 -*-
+def N():
+    print('Введите входные данные (3 числа)')
+    n = int(input('Введите количество долек по шир:'))
+    m = int(input('Введите количество долек по длине:'))
+    k = int(input('Введите количество долек:'))
+    if(n * m > k and (k % m == 0 or k % n == 0)):
+        return "Да"
+    else:
+        return "Нет"
+print(N())
