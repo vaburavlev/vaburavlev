067d9923efcfbc8966ec334ae51211691802bce7
vaburavlev <90718147+vaburavlev@users.noreply.github.com>
2021-10-04 15:00:08 +0300

    Исправленно

--- Задание 2 и скриншоты/Задание 1.6.py
+++ Задание 2 и скриншоты/Задание 1.6.py
@@ -1,37 +1,9 @@
-# --coding: utf -8 --
-a = int(input('Введите первое число'))
-b = int(input('Введите второе число'))
-c = int(input('Введите третье число'))
-d = int(input('Введите четвертое число'))
-x = a % 2
-y = b % 2
-z = c % 2
-f = d % 2
-if x==1 and y==1 and z==1 and f==1:
-    print('ДА')
-elif x==1 and y==1 and z==0 and f==0:
-    print('ДА')
-elif x==0 and y==0 and z==1 and f==1:
-    print('ДА')
-elif x==1 and y==0 and z==1 and f==0:
-    print('ДА')
-elif x==0 and y==1 and z==0 and f==1:
-    print('ДА')
-elif x==1 and y==1 and z==0 and f==1:
-    print('НЕТ')
-elif x==1 and y==1 and z==1 and f==0:
-    print('НЕТ')
-elif x==0 and y==0 and z==1 and f==0:
-    print('НЕТ')
-elif x==0 and y==0 and z==0 and f==1:
-    print('НЕТ')
-elif x==1 and y==0 and z==1 and f==1:
-    print('НЕТ')
-elif x==1 and y==0 and z==0 and f==1:
-    print('НЕТ')
-elif x==0 and y==1 and z==0 and f==0:
-    print('НЕТ')
-elif x==1 and y==0 and z==0 and f==1:
-    print('ДА')
-else: 
-    print('ДА')
+# -*- coding: utf-8 -*-
+a=int(input('Введите номер первой строки от 1 до 8:'))
+b=int(input('Введите номер первого столбца от 1 до 8:'))
+c=int(input('Введите номер второй строки от 1 до 8:'))
+d=int(input('Введите номер второго столбца от 1 до 8:'))
+if (a + b+ c + d) % 2 == 0:
+    print('Да')
+else:
+    print('Нет')
