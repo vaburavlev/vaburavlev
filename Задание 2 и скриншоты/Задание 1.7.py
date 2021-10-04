94feba5d6766105565b0f041dd6307e8b3c6135f
vaburavlev <90718147+vaburavlev@users.noreply.github.com>
2021-10-04 15:07:31 +0300

    Исправленно

--- Задание 2 и скриншоты/Задание 1.7.py
+++ Задание 2 и скриншоты/Задание 1.7.py
@@ -1,6 +1,9 @@
-# --coding: utf -8 --
-year = int(input('Ведите первый год'))
-if (year % 4 == 0 and year%100 != 0) or (year%400 == 0):
-    print("Высокосный")
-else:
-    print("Обычный")
+# -*- coding: utf-8 -*-
+def S():
+    print('Введите входные данные (1 число)')
+    year = int(input('Год  - '))
+    if((year % 4 == 0 and year % 100 != 0) or year % 400 == 0):
+        return "Да"
+    else:
+        return "Нет"
+print(S())
